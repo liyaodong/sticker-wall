2ff6026b02db8b71c8653461cba2655b15969853
Yaodong Li <ydli@thoughtworks.com>
2017-07-05 19:38:29 +0800

    Complete View engine

--- js/view.js
+++ js/view.js
@@ -1,3 +1,18 @@
+import { $$ } from './utils.js';
+
 export function render({ $el, $template, data }) {
-  $el.innerHTML = JSON.stringify(data);
+  $el.innerHTML = '';
+
+  const template = $template.content;
+
+  data.forEach(dataItem => {
+    const $item = document.importNode(template, true);
+    const $payloads = $item.querySelectorAll('[data-payload]');
+
+    $payloads.forEach($payload => {
+      $payload.innerHTML = dataItem[$payload.dataset.payload];
+    });
+
+    $el.appendChild($item);
+  });
 }
